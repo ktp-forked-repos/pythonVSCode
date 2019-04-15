0646ef46e725021e11aba756846004739549fee7
Don Jayamanne <don.jayamanne@yahoo.com>
2019-04-15 08:46:42 -0700

    Fixes

--- src/client/interpreter/display/interpreterSelectionTip.ts
+++ src/client/interpreter/display/interpreterSelectionTip.ts
@@ -6,7 +6,9 @@
 import { inject, injectable } from 'inversify';
 import { IExtensionActivationService } from '../../activation/types';
 import { IApplicationShell } from '../../common/application/types';
+import '../../common/extensions';
 import { IPersistentState, IPersistentStateFactory, Resource } from '../../common/types';
+import { swallowExceptions } from '../../common/utils/decorators';
 import { Common, Interpreters } from '../../common/utils/localize';
 
 @injectable()
@@ -16,17 +18,25 @@ export class InterpreterSelectionTip implements IExtensionActivationService {
         @inject(IPersistentStateFactory) private readonly factory: IPersistentStateFactory) {
         this.storage = this.factory.createGlobalPersistentState('InterpreterSelectionTip', false);
     }
+    /**
+     * Do not block this method waiting for user feedback, else this blocks loading the extension.
+     *
+     * @param {Resource} _resource
+     * @returns {Promise<void>}
+     * @memberof InterpreterSelectionTip
+     */
     public async activate(_resource: Resource): Promise<void> {
         if (this.storage.value) {
             return;
         }
-        await this.showTip();
+        this.showTip().ignoreErrors();
     }
+    @swallowExceptions('Failed to Display tip')
     private async showTip() {
         const selection = await this.shell.showInformationMessage(Interpreters.selectInterpreterTip(), Common.gotIt());
         if (selection !== Common.gotIt()) {
             return;
         }
-        this.storage.updateValue(true);
+        await this.storage.updateValue(true);
     }
 }
